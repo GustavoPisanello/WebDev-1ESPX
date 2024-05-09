39e8bbb9e1c52ad829bbe2b23ed3e7c562475b2d
GustavoPisanello <gustavo.pisanello@gmail.com>
2024-05-09 19:21:48 -0300

    Arrumado

--- Aula5/index.js
+++ Aula5/index.js
@@ -1,33 +1,41 @@
-const op = ["pedra", "papel", "tesoura"]
-let index = Math.floor(Math.random() * (3 - 0) ) + 0
-let choose = op[index]
-let chooseminus1 = ""
-
-if (choose = "pedra"){
-    chooseminus1 = op[2]
-}
-else{chooseminus1 = op[index - 1]}
-
 let result = document.getElementById("result")
 
-
 function Jokenpo(x){
+    const op = ["pedra", "papel", "tesoura"]
+    let index = Math.floor(Math.random() * (3 - 0) ) + 0
+    let choose = op[index]
+    let chooseminus1 = ""
+
+    if (choose == "pedra"){
+        chooseminus1 = op[2]
+    }
+
+    else {
+        chooseminus1 = op[index - 1]
+    }
+    result.innerHTML = calculo(x, choose, chooseminus1)
+}
+
+function calculo(x, choose, chooseminus1){
     console.log(`Eu ESCOLHI ${x}`)
-    console.log(`A MINHA ESCOLHA -1 ${chooseminus1}`)
     console.log(`A ESCOLHA DA MÁQUINA É ${choose}`)
-    
+    console.log(`A ESCOLHA DA MAQUINA -1 ${chooseminus1}`)
+    result.classList.remove("cinza")
+    result.classList.remove("vermelho")
+    result.classList.remove("verde")
+
     if (x == choose){
-       return (`Empate! Você escolheu ${x} e a máquina também!`)
+        result.classList.add("cinza")
+        return `Empate! Você escolheu ${x} e a máquina também!`
     }
     else{
-        if(x == chooseminus1){
-            return `Você ganhou! Você escolheu ${x} e a máquina ${choose}`
+        if (x == chooseminus1){
+            result.classList.add("vermelho")
+            return `Você perdeu! Escolhestes ${x} e a máquina ${choose}`
         }
         else{
-            return `Você perdeu! Você escolheu ${x} e a máquina ${choose}`
+            result.classList.add("verde")
+            return `Você venceu! Escolhestes ${x} e a máquina ${choose}`
         }
     }
-    
 }
-result.innerHTML = Jokenpo()
-
