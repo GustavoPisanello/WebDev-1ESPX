ccf18e9bd0dd02a9ef391fa9571e5a7471644d95
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 22:45:49 -0300

    react

---
+++ 2_SEM/Aula2/readme.md
@@ -0,0 +1,11 @@
+// Criar projeto node/react
+
+- npm create vite@latest
+
+// Instala as dependências do projeto
+- npm install (ou)
+- npm i
+
+// Executar o código
+
+- npm run dev 
---
+++ 2_SEM/Aula2/vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    const nome = "Giuseppe Camédia"
+
+    return ( 
+        <div>        
+            <h1>Olá, {nome} </h1>
+            
+        </div>
+    );
+}
+
+export default Header;
