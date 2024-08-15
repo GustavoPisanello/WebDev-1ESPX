a7032514f4b816f8c0baaa7863489f0e2d348240
Gustavo Pisanello <gustavo.pisanello@etec.sp.gov.br>
2024-08-15 14:24:55 -0300

    testando React

--- 2_SEM/Aula2/vite-project/src/App.jsx
+++ 2_SEM/Aula2/vite-project/src/App.jsx
@@ -1,10 +1,16 @@
+import Form from "./components/Form"
 import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <div className="content">
+        <div className="container">
+          <Header/>
+          <Form/>
+        </div>
+      </div>
     </>
   )
 }
