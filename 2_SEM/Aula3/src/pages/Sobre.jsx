468ed7234ddd1d8876a1f2391a441fc54c918ae2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 22:47:28 -0300

    Aula 03

---
+++ 2_SEM/Aula3/src/pages/Sobre.jsx
@@ -0,0 +1,27 @@
+function Sobre() {
+    return ( 
+        <>
+            <h1>Benjamin Arrola</h1>
+            <h1>Gisuseppe Camoles</h1>
+            <h1>Jacinto Leite Aquino Rego</h1>
+            <h1>Elma Maria</h1>
+            <h1>Deide Costa</h1>
+            <h1>Jalin Rabei</h1>
+            <h1>Kibe Shinha</h1>
+            <h1>Thomas Turbando</h1>
+            <h1>Shouta Nakama</h1>
+            <h1>Isadora Pinto</h1>
+            <h1>Paula Tejano</h1>
+            <h1>Oscar Alho</h1>
+            <h1>Cuca Beludo</h1>
+            <h1>Paula Noku</h1>
+            <h1>Mia Regaça</h1>
+            <h1>Caio Debocca</h1>
+            <h1>Tommy Lee Leite</h1>
+            <h1>Yasmin Asbola</h1>
+            <h1>Power Guido</h1>
+        </>
+    );
+}
+
+export default Sobre;
