fbd0b0c7dff23d15eb2ccfe858d2ffdc238ce364
giopolub <giovanni.polub.orlandi@gmail.com>
2024-09-04 22:50:07 -0300

    react é mt foda + tailwind ainda nem se fala

---
+++ 2_SEM/Aula4/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Index"
+
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+
+    </>
+  )
+}
+
+export default App
---
+++ 2_SEM/Aula4/vite-project/src/components/Header/Index.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Header(){
+    return(
+        <>
+            <h1 className="text-blue-800 text-5xl bg-red-950 m-5 flex">Título</h1>
+        </>
+    )
+}
